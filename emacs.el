c8911c66df327d425450ee54aabf6f3b813d7281
Yusuke Nomura <yunomu@gmail.com>
2013-06-14 16:12:34 +0900

    set golang tab-width

--- emacs.el
+++ emacs.el
@@ -1,3 +1,7 @@
 (keyboard-translate ?\C-h ?\C-?)
+(keyboard-translate ?\C-? ?\C-h)
+
 (setq backup-inhibited t)
 (setq inhibit-startup-message t)
+
+(add-hook 'go-mode-hook '(lambda () (setq tab-width 4)))
